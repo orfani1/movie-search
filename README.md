0f810ba033ecc66c8637f76d3b0dfaaf899645bd
orfani1 <bashir_orfani@yahoo.com>
2020-07-18 14:49:20 -0700

    Add README.md file

---
+++ README.md
@@ -0,0 +1,7 @@
+* My First JavaScript project
+
+__JavaScript/jQuery app that fetches movie data from the OMDb API
+
+_________ Usage 
+
+**** Run index.html on any server.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+/*arrow function and creating event */
 $(document).ready(() => {
   $('#searchForm').on('submit', (e) => {
+    /*variable */
     let searchText = $('#searchText').val();
     getMovies(searchText);
     e.preventDefault();
@@ -7,12 +9,15 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText){
-  axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=83e28115?s='+searchText)
+  /* Request API */
+  axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=83e28115&s='+searchText)
     .then((response) => {
       console.log(response);
       let movies = response.data.Search;
       let output = '';
+      /* jquery loop to iterate through each movie */
       $.each(movies, (index, movie) => {
+      /* putput variable*/
         output += `
           <div class="col-md-3">
             <div class="well text-center">
@@ -30,7 +35,7 @@ function getMovies(searchText){
       console.log(err);
     });
 }
-
+/*sessionStorage  pass data from one page to anther one*/ 
 function movieSelected(id){
   sessionStorage.setItem('movieId', id);
   window.location = 'movie.html';
@@ -39,13 +44,13 @@ function movieSelected(id){
 
 function getMovie(){
   let movieId = sessionStorage.getItem('movieId');
-
-  axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=83e28115?i='+movieId)
+  /* Request API */
+  axios.get('http://www.omdbapi.com/?tt3896198&apikey=83e28115&i='+movieId)
     .then((response) => {
       console.log(response);
       let movie = response.data;
-
-      let output =`
+      /* output Variable */
+      let output = `
         <div class="row">
           <div class="col-md-4">
             <img src="${movie.Poster}" class="thumbnail">
@@ -68,7 +73,7 @@ function getMovie(){
             <h3>Plot</h3>
             ${movie.Plot}
             <hr>
-            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="http://imdb.com?/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
             <a href="index.html" class="btn btn-default">Go Back To Search</a>
           </div>
         </div>
@@ -76,6 +81,8 @@ function getMovie(){
 
       $('#movie').html(output);
     })
+
+    /* catch error function*/
     .catch((err) => {
       console.log(err);
     });
